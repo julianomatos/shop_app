df2e7b66e0bdbe38ac5fb01bd8902f0b8acabc80
Juliano Ramos Matos <julianomatos.si@gmail.com>
2023-05-31 13:15:14 -0300

    first commit

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 import '../models/product.dart';
 
 class Products with ChangeNotifier {
-  List<Product> _items = [];
+  final List<Product> _items = [
+    Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red',
+      price: 29.99,
+      imageUrl: '',
+    ),
+  ];
 
   List<Product> get items {
     return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((p) => p.id == id);
   }
 
   void addProduct() {
--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,21 +1,8 @@
-
 import 'package:flutter/material.dart';
-import 'package:shop_app/widgets/product_item.dart';
-import '../models/product.dart';
+import 'package:shop_app/screens/products_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   ProductOverviewScreen({super.key});
-
-  final List<Product> loadedProducts = [
-    // ignore: non_constant_identifier_names
-    Product(
-        id: 'p1',
-        title: 'Red Shirt',
-        description: 'A red shirt - it is pretty red',
-        price: 29.99,
-        imageUrl: '',
-        ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,18 +10,7 @@ class ProductOverviewScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('My Shop'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10), 
-        itemBuilder: (ctx, i) => ProductItem(
-          id: loadedProducts[i].id, 
-          title: loadedProducts[i].title, 
-          imageUrl: loadedProducts[i].imageUrl,)),
+      body: ProductsGrid(),
     );
   }
 }
